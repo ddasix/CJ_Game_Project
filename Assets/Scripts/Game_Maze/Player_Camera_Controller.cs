5be0769f447f2c62b403350748d32d8870571915
ddasix <ddasix55@gmail.com>
2018-06-16 16:03:49 +0900

    dd

---
+++ Assets/Scripts/Game_Maze/Player_Camera_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Camera_Controller : MonoBehaviour {
+
+	public RotationAxis m_Axis = RotationAxis.eMouseX;
+	public float m_Speed_Horizontal = 10.0f;
+	public float m_Speed_Vertical = 10.0f;
+
+	public float m_MaximumVert = 45.0f;
+	public float m_MinimumVert = -45.0f;
+
+	public float m_RotationX = 0;
+
+	public enum RotationAxis {
+		eMouseX = 1,
+		eMouseY = 2
+	}
+
+	// Update is called once per frame
+	private void Update () {
+		if(m_Axis == RotationAxis.eMouseX) {
+			transform.Rotate(0, Input.GetAxis ("Mouse X") * m_Speed_Horizontal, 0);
+		} else if(m_Axis == RotationAxis.eMouseY) {
+			m_RotationX -= Input.GetAxis("Mouse Y") * m_Speed_Vertical;
+			m_RotationX = Mathf.Clamp(m_RotationX, m_MinimumVert, m_MaximumVert);
+
+			transform.localEulerAngles = new Vector3(m_RotationX,0,0);
+		}
+	}
+}
---
+++ Assets/Scripts/MainMenu_Start.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_Start : MonoBehaviour {
+
+	public void Button_Start() {
+		SceneManager.LoadScene ("Scene_Maze_InGame");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
